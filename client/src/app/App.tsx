70d209cbf5456e357a17b16b374a5a0e00117c01
Khalid Hamour <khalid.hamour99@gmail.com>
2022-03-04 17:58:51 -0500

    client: -created settings page -moved nav folder into layout folder -changed section component dividers to be "middle" variant

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -2,6 +2,7 @@ import Layout from "../features/Layout/Layout";
 import Auth from "../pages/AuthPage/Auth";
 import ProjectsPage from "../pages/ProjectsPage/ProjectsPage";
 import ProjectOverview from "../pages/ProjectOverviewPage/ProjectOverview";
+import SettingsPage from "../pages/SettingsPage/settingsPage";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { StyledEngineProvider } from "@mui/material/styles";
@@ -24,7 +25,14 @@ function App() {
                 </Layout>
               }
             />
-            <Route path="/:user/settings" element={<Layout></Layout>} />
+            <Route
+              path="/:user/settings"
+              element={
+                <Layout>
+                  <SettingsPage />
+                </Layout>
+              }
+            />
             <Route
               path="/:user/projects/:projectName/:projectId"
               element={
--- client/src/pages/SettingsPage/settingsPage.tsx
+++ client/src/pages/SettingsPage/settingsPage.tsx
@@ -1,5 +1,20 @@
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import Section from "../../features/common/Section/Section";
+
+import "./settingsPage.css";
+
 const settingsPage = () => {
-  return <div>settingsPage</div>;
+  return (
+    <>
+      <Grid item xs={12} className={"settings-page-container"}>
+        <Section variant={"TOP"} sectionHeading={"GENERAL"}>
+          <Typography></Typography>
+        </Section>
+        <Section variant={"BOTTOM"} sectionHeading={""}></Section>
+      </Grid>
+    </>
+  );
 };
 
 export default settingsPage;
